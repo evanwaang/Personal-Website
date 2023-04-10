94328dbd2aeeecfee8c39f5d9a6163432d23df88
evanwang <evanwaang2020@gmail.coim>
2023-04-10 10:51:44 -0700

    Updates

---
+++ precache-manifest.6988cfab7b774fa3716178311ef99807.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c16bbfb738a07f5fe120",
+    "url": "/Personal-Website/static/css/main.97778629.chunk.css"
+  },
+  {
+    "revision": "c16bbfb738a07f5fe120",
+    "url": "/Personal-Website/static/js/main.c16bbfb7.chunk.js"
+  },
+  {
+    "revision": "2556a7efe832936860cc",
+    "url": "/Personal-Website/static/js/1.2556a7ef.chunk.js"
+  },
+  {
+    "revision": "87f34ce215affaea4209",
+    "url": "/Personal-Website/static/js/runtime~main.87f34ce2.js"
+  },
+  {
+    "revision": "dcfb8790d3780f0526bc2a98061d1880",
+    "url": "/Personal-Website/static/media/Me.dcfb8790.png"
+  },
+  {
+    "revision": "a9f82747c3450f92609f275efc7b595c",
+    "url": "/Personal-Website/static/media/HomePhoto.a9f82747.png"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/Personal-Website/static/media/stars.6494ab5b.png"
+  },
+  {
+    "revision": "c5f5f3f9e880b7ff9d5085b3ce92ad3d",
+    "url": "/Personal-Website/index.html"
+  }
+];
